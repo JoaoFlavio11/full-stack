de1c0cc227ccba0bb61491539952b7204926640c
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-03-16 15:11:03 -0300

    finalizando rest api

--- api-rest/src/controllers/products-controller.ts
+++ api-rest/src/controllers/products-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AppError } from "../utils/app-error";
+import {z} from "zod"
 
 class ProductsController{
   /*
@@ -17,23 +18,37 @@ class ProductsController{
   }
 
   create(req: Request, res: Response){
-    const {name, price} = req.body
-    
-    if(!name){
-      throw new AppError("o nome do produto é obrigatório.", 400);
-    }
-    
-    if(name.trim().length < 6){
-      throw new AppError("nome com pelo menos 6 caracteres")
-    }
 
-    if(!price){
-      throw new AppError("o preço do produto é obrigatório", 400);
-    }
+    const bodySchema = z.object({
+      name: z
+        .string({required_error:"Name is required!"})
+        .trim()
+        .email()
+        .min(6,{message: "Name must have 6 or more chars"})
+        .max(12), 
+      price: z
+        .number({required_error:"Price is required!"})
+        .positive({message:"Price must be positive"})
+        .gte(1),
+    })
 
-    if(price < 0){
-      throw new AppError("o preço do produto não pode ser um valor negativo", 400);
-    }
+    const {name, price} = bodySchema.parse(req.body)
+
+  //   if(!name){
+  //     throw new AppError("o nome do produto é obrigatório.", 400);
+  //   }
+    
+  //   if(name.trim().length < 6){
+  //     throw new AppError("nome com pelo menos 6 caracteres")
+  //   }
+
+  //   if(!price){
+  //     throw new AppError("o preço do produto é obrigatório", 400);
+  //   }
+
+  //   if(price < 0){
+  //     throw new AppError("o preço do produto não pode ser um valor negativo", 400);
+  //   }
 
     //throw new Error("erro ao tentar criar produto");
     //throw new AppError("erro ao tentar criar produto");
--- api-rest/src/server.ts
+++ api-rest/src/server.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes/index";
 import { AppError } from "./utils/app-error";
+import { ZodError } from "zod";
 
 const PORT = 3333
 
@@ -20,9 +21,14 @@ app.get("/", (req:Request , res: Response) => {
 */
 
 app.use((error: any, req: Request, res: Response, next:NextFunction) => {
+
   if(error instanceof AppError){
     return res.status(error.statusCode).json({ message: error.message})
   }
+
+  if( error instanceof ZodError){
+    res.status(400).json({ message: "validation error!", issues: error.format()})
+  } 
 
   res.status(500).json({message: error.message})
 })
