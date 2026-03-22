a16912baf9e216af8ca4173e8da608cd5dd6e583
João Flávio C. Lopes <joaoflaviocl@gmail.com>
2026-03-22 15:58:06 -0300

    .

---
+++ api-restaurant/src/controllers/productsControllers.ts
@@ -0,0 +1,13 @@
+import type { NextFunction, Request, Response } from "express"
+
+class ProductController {
+  async index(req: Request, res: Response, next: NextFunction){
+    try {
+      return res.json({message: "ok"})
+    } catch(error) {
+      next(error)
+    }
+  }
+}
+
+export {ProductController}
---
+++ api-restaurant/src/middlewares/errorHandligs.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction, response } from "express";
+
+import { AppError } from "../utils/appError";
+
+export function errorHandling(error: any, req: Request, res: Response, next: NextFunction) {
+  if(error instanceof AppError){
+    return res.status(error.statusCode).json({message: error.message})
+  }
+
+  return response.status(500).json({message: error.message})
+}
