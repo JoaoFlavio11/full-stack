c9e9606f0a63c6fdf3fd15627bcc56124cbd01c0
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-03-25 17:12:53 -0300

    docker

--- nodeJS/2-api-rest/src/routes/productsRoutes.ts
+++ nodeJS/2-api-rest/src/routes/productsRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express"
 import { ProductController } from "../controllers/productsControllers";
 
-const productsRoutes = Router()
+export const productsRoutes = Router()
 const productsController = new ProductController()
 
 productsRoutes.get("/", productsController.index)
 productsRoutes.post("/", productsController.create)
 productsRoutes.put("/:id", productsController.update)
 productsRoutes.delete("/:id", productsController.remove)
-
-export {productsRoutes};
