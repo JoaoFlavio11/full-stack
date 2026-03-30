eac188a0ab2c3d1df89c1f8ae80e97c2c1f3defe
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-03-30 10:11:02 -0300

    fim modulo tests

--- tests/src/sum.test.ts
+++ tests/src/sum.test.ts
@@ -1,5 +1,26 @@
-import test from "node:test";
 
-test("example", () => {
-  console.log("test OK")
-});
+import sum from "./sum.ts";
+
+describe("sum", () => {
+  
+  beforeAll(() => {
+    console.log("função antes dos testes")
+  })
+
+  it("sum", () => {
+    const result = sum(3,7);
+
+    expect(result).toBe(10)
+  });
+
+  test("sum of 2+2 must be 4", () => {
+    const result = sum(2,2);
+
+    expect(result).toBe(4)
+  })
+
+  afterAll(() => {
+    console.log("função depois dos testes")
+  })
+})
+
