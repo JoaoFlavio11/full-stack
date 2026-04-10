1cdfd56a8a505c61a3936f3165fde141ce38883f
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-04-10 16:43:25 -0300

    test

--- projects/rocket-log/src/tests/UserController.test.ts
+++ projects/rocket-log/src/tests/UserController.test.ts
@@ -1,7 +1,17 @@
 import { describe } from "node:test"
+import request from "supertest";
+import { app } from "@/app.js";
 
 describe("UsersController", () => {
-  it("test", () => {
-    console.log("passou por aqui")
+  it("shoud create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "test",
+      email: "test@test.com",
+      password: "test123"
+    })
+
+    expect(response.status).toBe(201)
+    expect(response.body).toHaveProperty("id")
+    expect(response.body.name).toBe("Test User")
   })
 })
