65323b8b15da58ce787e5f7eaf4167c5fe8944a2
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-04-13 16:18:11 -0300

    fim teste

--- projects/rocket-log/src/tests/UserController.test.ts
+++ projects/rocket-log/src/tests/UserController.test.ts
@@ -1,8 +1,15 @@
 import { describe } from "node:test"
 import request from "supertest";
 import { app } from "@/app.js";
+import { prisma } from "@/database/prisma.js";
 
 describe("UsersController", () => {
+  let user_id: string
+
+  afterAll(async () => {
+    await prisma.user.delete({where: {id: user_id} })
+  })
+
   it("shoud create a new user successfully", async () => {
     const response = await request(app).post("/users").send({
       name: "test",
@@ -13,5 +20,28 @@ describe("UsersController", () => {
     expect(response.status).toBe(201)
     expect(response.body).toHaveProperty("id")
     expect(response.body.name).toBe("Test User")
+
+    user_id = response.body.id
+  })
+
+  it("should throw an error if user with same email already exists", async () => {
+    const response = await request(app).post("/users").send({
+      name: "duplicate email",
+      email: "test@test.com",
+      password: "test123"
+    })
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe("User with same email already exists")
+  })
+
+  it("shoud throw a validation error if email is invalid", async () => {
+    const response = (await request(app).post("/users")).setEncoding({
+      name:"test user",
+      email:"invalid",
+      password:"password"
+    })
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe("validation error")
   })
 })
