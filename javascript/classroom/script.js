7747c615cb7e5e8daaec7cec512e63c83654c265
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-04-16 17:25:55 -0300

    vars js

--- javascript/classroom/script.js
+++ javascript/classroom/script.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+// var ou let = variaveis js que trocam de valor.
+// const = n varia.
+
+// var aluno -> só declara.
+// var aluno = 'joao' -> declara e atribui.
+
+// aluno = 'jf' -> troca o valor
+
+// CaseSensitive => joao != Joao
+
+// console.log(aluno) = jf
