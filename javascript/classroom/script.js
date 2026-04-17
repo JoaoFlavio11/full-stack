49d6157a4be5d23b8ffea8bab6d9c8a48bedb367
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-04-17 17:00:35 -0300

    types js

--- javascript/classroom/script.js
+++ javascript/classroom/script.js
@@ -11,3 +11,19 @@ console.log("Hello World!");
 // CaseSensitive => joao != Joao
 
 // console.log(aluno) = jf
+
+// typeof = mostra o tipo do dado
+// string = 'palavra' ou "frases" e => ' "" ' ou " '' " ou ``
+//   | concat: 'ola'+user+'tudo bem?'
+//   | template literals: `olá, ${user}`
+
+// number = 222, + ou -, ou 0.5..., 
+//   | NaN = not a number
+
+// boolean = true ou false
+
+// null = vazio
+// undefined = var sem tipo e automático a uma variável sem valor
+
+// convesão/casting => feito conciente | Number(valor ou var) | String()... / var.toString 
+// e coerção => feito implicitamente pelo js | EX: 9 + "9" = 18
