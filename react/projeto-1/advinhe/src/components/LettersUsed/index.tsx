7076e4261928a127ef406bdcfee3cfdb373ac938
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-04-23 15:09:18 -0300

    fim projeto advinhe

--- react/projeto-1/advinhe/src/components/LettersUsed/index.tsx
+++ react/projeto-1/advinhe/src/components/LettersUsed/index.tsx
@@ -1,13 +1,30 @@
-import { Letter } from "../Letter";
 import styles from "./styles.module.css";
 
-export function LettersUsed() {
+import { Letter } from "../Letter";
+
+export type LettersUsedProps = {
+  value: string;
+  correct: boolean;
+};
+
+type Props = {
+  data: LettersUsedProps[];
+};
+
+export function LettersUsed({ data }: Props) {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras utilizadas</h5>
-       <div>
-        <Letter value="R" size="small" color="correct" />
-        <Letter value="X" size="small" color="wrong" />
+
+      <div>
+        {data.map(({ value, correct }) => (
+          <Letter
+            key={value}
+            value={value}
+            size="small"
+            color={correct ? "correct" : "wrong"}
+          />
+        ))}
       </div>
     </div>
   );
